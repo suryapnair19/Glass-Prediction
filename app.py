322e73bc0a76b13875278a91abc2c03b168b1758
suryapnair19 <suryapnair19@gmail.com>
2024-11-17 18:01:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-model = pickle.load(open('random_forest_model.pkl', 'rb'))
+model = pickle.load(open('modelpro.pkl', 'rb'))
 # Glass types corresponding to class numbers
 glass_types = {
     1: 'Building Windows Float Processed',
